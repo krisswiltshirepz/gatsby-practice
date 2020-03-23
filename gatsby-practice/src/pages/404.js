55b975fc0cebf01c87e1d22b6f9ca00c0dea555e
krisswiltshirepz <kriss@parentzone.org.uk>
2020-03-23 17:09:17 +0000

    404 page

---
+++ gatsby-practice/src/pages/404.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class err extends Component {
+  render() {
+    return (
+      <div>
+        Ah snap! nothing here
+        404
+      </div>
+    );
+  }
+}
